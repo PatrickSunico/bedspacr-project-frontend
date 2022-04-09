d36ef4d8d80eb094460c4191db78382929bdcaa2
PatrickSunico <apatricksunico@gmail.com>
2022-04-10 02:17:14 +0300

    checkpoint

--- src/components/utils/ProtectedRoutes/ProtectedRoutes.js
+++ src/components/utils/ProtectedRoutes/ProtectedRoutes.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Outlet, Navigate, Redirect, useNavigate } from "react-router-dom";
 import { authData } from "../../../features/auth/authSlice";
 
 const ProtectedRoutes = () => {
  const data = useSelector(authData);
+ const navigate = useNavigate();
  const { user } = data;
+
+ useEffect(() => {
+  if (!user && user == null) {
+   console.log("Hello");
+   navigate("/login");
+   clearInterval(intervalId);
+  }
+ }, [user, navigate]);
 
  return user && user != null ? <Outlet /> : <Navigate to="/login" replace />;
 };
